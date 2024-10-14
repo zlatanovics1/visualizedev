8e4bdfb220093ac655f27c5189096f4d7a7d835e
zlatanovics1 <office@zlatanovics.com>
2024-10-14 21:01:51 +0200

    fix: OAuth redirect

--- app/actions/auth/clientActions.ts
+++ app/actions/auth/clientActions.ts
@@ -2,11 +2,15 @@ import { createClient } from "@/database/supabase/client";
 
 export async function loginWithOAuth(provider: "google" | "github") {
   try {
+    const isLocal = process.env.NODE_ENV === "development";
+    const redirectUrl = isLocal
+      ? "http://localhost:3000"
+      : "https://visualizedev.app";
     const client = createClient();
     const res = await client.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+        redirectTo: `${redirectUrl}/auth/callback`,
       },
     });
   } catch (e) {
